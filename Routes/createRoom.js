995a2624034e6f62c35e79e2f4a1049975d54eaa
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2024-01-14 18:24:12 +0530

    trycatch added

--- Routes/createRoom.js
+++ Routes/createRoom.js
@@ -10,26 +10,31 @@ module.exports = function (io) {
     // For Createroom
 
     Router.get("/createRoom/", async (req, res) => {
-        let html = readFileSync(path.join(__dirname, '../static', 'createRoom.html'), "utf-8")
-        let existingRoomsList = await SavingData.find(
-            {},
-            { _id: false },
-            {
-                projection: {
-                    // _id: 0,
-                    RandNo: 1
-                }
+        try {
+
+            let html = readFileSync(path.join(__dirname, '../static', 'createRoom.html'), "utf-8")
+            let existingRoomsList = await SavingData.find(
+                {},
+                { _id: false },
+                {
+                    projection: {
+                        // _id: 0,
+                        RandNo: 1
+                    }
+                })
+            let existinglist = existingRoomsList.map((element) => {
+                return element.RandNo
             })
-        let existinglist = existingRoomsList.map((element) => {
-            return element.RandNo
-        })
-        let randomNumber = Math.floor((Math.random() * 1000000) + 1);
-        while (existinglist.includes(randomNumber)) {
-            randomNumber = Math.floor((Math.random() * 1000000) + 1);
+            let randomNumber = Math.floor((Math.random() * 1000000) + 1);
+            while (existinglist.includes(randomNumber)) {
+                randomNumber = Math.floor((Math.random() * 1000000) + 1);
+            }
+            // while(randomNumber in existingRoomsList[])
+            html = html.replace("ABHINAVYADAV", randomNumber)
+            return res.send(html)
+        } catch (error) {
+            return res.status(500).json({ success: false, error: "Internal Server Error " })
         }
-        // while(randomNumber in existingRoomsList[])
-        html = html.replace("ABHINAVYADAV", randomNumber)
-        res.send(html)
     })
     Router.post("/createRoom/demo", async (req, res) => {
         try {
@@ -59,31 +64,37 @@ module.exports = function (io) {
 
     Router.post("/chatting/", async (req, res) => {
         let html = readFileSync(path.join(__dirname, '../static', 'chatting.html'), "utf-8")
-        res.send(html)
+        return res.send(html)
     })
     Router.get("/chatting/", async (req, res) => {
         let html = readFileSync(path.join(__dirname, '../static', 'chatting.html'), "utf-8")
-        res.send(html)
+        return res.send(html)
     })
 
 
     // For JoinRoom
     Router.get("/joinRoom/", async (req, res) => {
         let html = readFileSync(path.join(__dirname, '../static', 'joinroom.html'), "utf-8")
-        res.send(html)
+        return res.send(html)
     })
 
     Router.post("/joinRoom/demo", async (req, res) => {
-        if (!req.body.roomId) {
-            return res.status(400).send({ success: false, error: "please enter the roomid" })
-        }
-        let existingRoom = await SavingData.findOne({ RandNo: req.body.roomId }).select("_id")
-        // console.log(req.body,existingRoom)
-        if (!existingRoom) {
-            return res.status(400).send({ success: false, error: "Please create a room!" })
-        }
+        try {
 
-        return res.cookie("room", req.body.roomId).redirect("/chatting")
+            if (!req.body.roomId) {
+                return res.status(400).send({ success: false, error: "please enter the roomid" })
+            }
+            let existingRoom = await SavingData.findOne({ RandNo: req.body.roomId }).select("_id")
+            // console.log(req.body,existingRoom)
+            if (!existingRoom) {
+                return res.status(400).send({ success: false, error: "Please create a room!" })
+            }
+
+            return res.cookie("room", req.body.roomId).redirect("/chatting")
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ success: false, error: "internal Server error!" })
+        }
     })
 
 
